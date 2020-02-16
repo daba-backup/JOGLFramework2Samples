1bc96dfde1c892b2b814cffbde392ebfb8dc31f4
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-02-16 21:07:19 +0900

    Added a sample for perspective shadow mapping.

---
+++ src/com/daxie/testspace/joglf/g2/shadow_mapping/Shader/shadow_mapping_2/draw_vshader.glsl
@@ -0,0 +1,63 @@
+#version 330
+
+layout(location=0) in vec3 vs_in_position;
+layout(location=1) in vec2 vs_in_uv;
+layout(location=2) in vec3 vs_in_normal;
+
+uniform vec3 camera_position;
+uniform vec3 camera_target;
+uniform mat4 projection;
+uniform mat4 view_transformation;
+uniform float camera_near;
+uniform float camera_far;
+
+uniform vec3 light_direction;
+uniform vec4 ambient_color;
+
+uniform float diffuse_power;
+uniform float specular_power;
+
+uniform float fog_start;
+uniform float fog_end;
+
+uniform mat4 depth_bias_mvp;
+
+out vec2 vs_out_uv;
+out vec4 vs_out_color;
+out float vs_out_fog_factor;
+
+out vec4 shadow_coords;
+
+void SetPosition(){
+    mat4 camera_matrix=projection*view_transformation;
+    gl_Position=camera_matrix*vec4(vs_in_position,1.0);
+
+    shadow_coords=depth_bias_mvp*vec4(vs_in_position,1.0);
+}
+void SetUVs(){
+    vs_out_uv=vs_in_uv;
+}
+void SetLighting(){
+    vec3 camera_direction=normalize(camera_target-camera_position);
+    vec3 half_le=-normalize(camera_direction+light_direction);
+
+    float diffuse=clamp(dot(vs_in_normal,-light_direction),0.0,1.0);
+    float specular=pow(clamp(dot(vs_in_normal,half_le),0.0,1.0),2.0);
+
+    vec4 diffuse_color=vec4(diffuse*diffuse_power);
+    vec4 specular_color=vec4(specular*specular_power);
+
+    vs_out_color=ambient_color+diffuse_color+specular_color;
+    vs_out_color.a=1.0;
+}
+void SetFog(){
+    float linear_pos=length(camera_position-vs_in_position);
+    vs_out_fog_factor=clamp((fog_end-linear_pos)/(fog_end-fog_start),0.0,1.0);
+}
+
+void main(){
+    SetPosition();
+    SetUVs();
+    SetLighting();
+    SetFog();
+}
---
+++ src/com/daxie/testspace/joglf/g2/shadow_mapping/ShadowMappingTestWindow2.java
@@ -0,0 +1,144 @@
+package com.daxie.testspace.joglf.g2.shadow_mapping;
+
+import java.nio.IntBuffer;
+
+import com.daxie.basis.matrix.Matrix;
+import com.daxie.basis.matrix.MatrixFunctions;
+import com.daxie.basis.vector.Vector;
+import com.daxie.basis.vector.VectorFunctions;
+import com.daxie.joglf.gl.front.CameraFront;
+import com.daxie.joglf.gl.front.FogFront;
+import com.daxie.joglf.gl.front.LightingFront;
+import com.daxie.joglf.gl.model.Model3D;
+import com.daxie.joglf.gl.shader.GLShaderFunctions;
+import com.daxie.joglf.gl.shader.ShaderProgram;
+import com.daxie.joglf.gl.tool.matrix.ProjectionMatrixFunctions;
+import com.daxie.joglf.gl.tool.matrix.TransformationMatrixFunctions;
+import com.daxie.joglf.gl.window.JOGLFWindow;
+import com.daxie.joglf.gl.wrapper.GLWrapper;
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL4;
+
+public class ShadowMappingTestWindow2 extends JOGLFWindow{
+	private int fbo_id;
+	private int texture_id;
+	
+	private static final int SHADOW_WIDTH=2048;
+	private static final int SHADOW_HEIGHT=2048;
+	
+	private int model_handle;
+	
+	private ShaderProgram depth_program;
+	private ShaderProgram draw_program;
+
+	@Override
+	protected void Init() {
+		//Set up the program.
+		GLShaderFunctions.CreateProgram(
+				"depth", 
+				"./Data/Shader/330/shadow_mapping_2/depth_vshader.glsl",
+				"./Data/Shader/330/shadow_mapping_2/depth_fshader.glsl");
+		GLShaderFunctions.CreateProgram(
+				"draw", 
+				"./Data/Shader/330/shadow_mapping_2/draw_vshader.glsl",
+				"./Data/Shader/330/shadow_mapping_2/draw_fshader.glsl");
+		depth_program=new ShaderProgram("depth");
+		draw_program=new ShaderProgram("draw");
+		
+		//Set up a framebuffer and a depth texture.
+		IntBuffer fbo_ids=Buffers.newDirectIntBuffer(1);
+		IntBuffer texture_ids=Buffers.newDirectIntBuffer(1);
+		
+		GLWrapper.glGenFramebuffers(1, fbo_ids);
+		GLWrapper.glGenTextures(1, texture_ids);
+		fbo_id=fbo_ids.get(0);
+		texture_id=texture_ids.get(0);
+		
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, texture_id);
+		GLWrapper.glTexImage2D(
+				GL4.GL_TEXTURE_2D, 0,GL4.GL_DEPTH_COMPONENT, 
+				SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL4.GL_DEPTH_COMPONENT, GL4.GL_FLOAT, null);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAG_FILTER, GL4.GL_NEAREST);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MIN_FILTER, GL4.GL_NEAREST);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_S, GL4.GL_CLAMP_TO_EDGE);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_T, GL4.GL_CLAMP_TO_EDGE);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
+		
+		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, fbo_id);
+		GLWrapper.glFramebufferTexture2D(
+				GL4.GL_FRAMEBUFFER, GL4.GL_DEPTH_ATTACHMENT, 
+				GL4.GL_TEXTURE_2D, texture_id, 0);
+		if(GLWrapper.glCheckFramebufferStatus(GL4.GL_FRAMEBUFFER)!=GL4.GL_FRAMEBUFFER_COMPLETE) {
+			System.out.println("Error:Incomplete framebuffer");
+		}
+		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, 0);
+		
+		//Load a model.
+		model_handle=Model3D.LoadModel("./Data/Model/OBJ/Plane/plane.obj");
+		Model3D.RescaleModel(model_handle, VectorFunctions.VGet(0.1f, 0.1f, 0.1f));
+		Model3D.RemoveAllPrograms(model_handle);
+		Model3D.AddProgram(model_handle, "draw");
+		
+		//Set up fronts.
+		CameraFront.AddProgram("draw");
+		FogFront.AddProgram("draw");
+		LightingFront.AddProgram("draw");
+	}
+	
+	@Override
+	protected void Update() {
+		Vector light_position=VectorFunctions.VGet(-20.0f, 20.0f, 20.0f);
+		Vector light_target=VectorFunctions.VGet(0.0f, 0.0f, 0.0f);
+		
+		Matrix projection=ProjectionMatrixFunctions.GetPerspectiveMatrix(20.0f, 1.0f, 1.0f, 100.0f);
+		Matrix transformation=
+				TransformationMatrixFunctions.GetViewTransformationMatrix(
+						light_position, light_target, VectorFunctions.VGet(0.0f, 1.0f, 0.0f));
+		Matrix depth_mvp=MatrixFunctions.MMult(projection, transformation);
+		
+		Matrix bias=new Matrix();
+		bias.SetValue(0, 0, 0.5f);
+		bias.SetValue(0, 3, 0.5f);
+		bias.SetValue(1, 1, 0.5f);
+		bias.SetValue(1, 3, 0.5f);
+		bias.SetValue(2, 2, 0.5f);
+		bias.SetValue(2, 3, 0.5f);
+		bias.SetValue(3, 3, 1.0f);
+		
+		Matrix depth_bias_mvp=MatrixFunctions.MMult(bias, depth_mvp);
+		
+		depth_program.Enable();
+		depth_program.SetUniform("depth_mvp", true, depth_mvp);
+		depth_program.Disable();
+		
+		draw_program.Enable();
+		draw_program.SetUniform("depth_bias_mvp", true, depth_bias_mvp);
+		draw_program.Disable();
+		
+		CameraFront.SetCameraPositionAndTarget_UpVecY(
+				VectorFunctions.VGet(20.0f, 30.0f, 20.0f), VectorFunctions.VGet(0.0f, 0.0f, 0.0f));
+		LightingFront.SetLightDirection(light_position, light_target);
+	}
+	
+	@Override
+	protected void Draw() {
+		depth_program.Enable();
+		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, fbo_id);
+		GLWrapper.glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		GLWrapper.glClear(GL4.GL_DEPTH_BUFFER_BIT);
+		GLWrapper.glCullFace(GL4.GL_FRONT);
+		Model3D.TransferModel(model_handle);
+		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, 0);
+		depth_program.Disable();
+		
+		draw_program.Enable();
+		GLWrapper.glCullFace(GL4.GL_BACK);
+		GLWrapper.glViewport(0, 0, this.GetWidth(), this.GetHeight());
+		GLWrapper.glActiveTexture(GL4.GL_TEXTURE1);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, texture_id);
+		draw_program.SetUniform("shadow_map", 1);
+		Model3D.DrawModel(model_handle,0,"texture_sampler");
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
+		draw_program.Disable();
+	}
+}
