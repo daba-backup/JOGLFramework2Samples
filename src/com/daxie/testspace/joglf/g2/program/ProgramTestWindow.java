c3e122d539b63352c75f2985c96323182d0ed1be
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-02-20 10:11:19 +0900

    Added a sample.

---
+++ src/com/daxie/testspace/joglf/g2/program/ProgramTestWindow.java
@@ -0,0 +1,35 @@
+package com.daxie.testspace.joglf.g2.program;
+
+import com.daxie.joglf.gl.draw.GLDrawFunctions2D;
+import com.daxie.joglf.gl.shader.GLShaderFunctions;
+import com.daxie.joglf.gl.shader.ShaderProgram;
+import com.daxie.joglf.gl.window.JOGLFWindow;
+
+public class ProgramTestWindow extends JOGLFWindow{
+	private ShaderProgram program;
+	
+	@Override
+	protected void Init() {
+		GLShaderFunctions.CreateProgram(
+				"program", 
+				"./Data/Shader/330/program/vshader.glsl",
+				"./Data/Shader/330/program/fshader.glsl");
+		program=new ShaderProgram("program");
+	}
+	
+	@Override
+	protected void Update() {
+		program.Enable();
+		program.SetUniform("i", 50);
+		program.SetUniform("f", 0.5f);
+		program.SetUniform("v2", 1.0f, 1.0f);
+		program.SetUniform("v3", 1.0f, 1.0f, 1.0f);
+		program.SetUniform("v4", 1.0f, 0.0f, 1.0f, 1.0f);
+	}
+	
+	@Override
+	protected void Draw() {
+		program.Enable();
+		GLDrawFunctions2D.TransferFullscreenQuad();
+	}
+}
