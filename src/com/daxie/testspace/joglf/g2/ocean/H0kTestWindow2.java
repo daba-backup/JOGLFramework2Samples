25cf8f5a27ca86013e6637b8ccb5f30108583f60
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-02-21 23:29:48 +0900

    Added a sample to simulate ocean waves.

--- src/com/daxie/testspace/joglf/g2/ocean/H0kTestWindow2.java
+++ src/com/daxie/testspace/joglf/g2/ocean/H0kTestWindow2.java
@@ -1,4 +1,4 @@
-package com.daxie.testspace.joglf.g2.box_muller;
+package com.daxie.testspace.joglf.g2.ocean;
 
 import java.io.IOException;
 import java.nio.Buffer;
@@ -17,25 +17,26 @@ import com.daxie.tool.FileFunctions;
 import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.GL4;
 
-public class BoxMullerMethodWindow extends JOGLFWindow{
+public class H0kTestWindow2 extends JOGLFWindow{
 	private int fbo_id;
+	private int[] output_texture_ids;
 	private int input_texture_id;
-	private int output_texture_id;
 	
-	private static final int TEXTURE_WIDTH=256;
-	private static final int TEXTURE_HEIGHT=256;
+	private static final int TEXTURE_WIDTH=512;
+	private static final int TEXTURE_HEIGHT=512;
 	
 	private ShaderProgram program;
 	
 	@Override
 	protected void Init() {
 		IntBuffer fbo_ids=Buffers.newDirectIntBuffer(1);
-		IntBuffer texture_ids=Buffers.newDirectIntBuffer(2);
+		IntBuffer texture_ids=Buffers.newDirectIntBuffer(3);
 		GLWrapper.glGenFramebuffers(1, fbo_ids);
-		GLWrapper.glGenTextures(2, texture_ids);
+		GLWrapper.glGenTextures(3, texture_ids);
+		
 		fbo_id=fbo_ids.get(0);
-		input_texture_id=texture_ids.get(0);
-		output_texture_id=texture_ids.get(1);
+		output_texture_ids=new int[] {texture_ids.get(0),texture_ids.get(1)};
+		input_texture_id=texture_ids.get(2);
 		
 		this.SetupInputTexture();
 		this.SetupOutputTexture();
@@ -64,21 +65,28 @@ public class BoxMullerMethodWindow extends JOGLFWindow{
 		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
 	}
 	private void SetupOutputTexture() {
-		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, output_texture_id);
-		GLWrapper.glTexImage2D(
-				GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA32F, 
-				TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL4.GL_RGBA, GL4.GL_FLOAT, null);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAG_FILTER, GL4.GL_NEAREST);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MIN_FILTER, GL4.GL_NEAREST);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_S, GL4.GL_CLAMP_TO_EDGE);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_T, GL4.GL_CLAMP_TO_EDGE);
-		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
+		for(int i=0;i<2;i++) {
+			GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, output_texture_ids[i]);
+			GLWrapper.glTexImage2D(
+					GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA32F, 
+					TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL4.GL_RGBA, GL4.GL_FLOAT, null);
+			GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAG_FILTER, GL4.GL_NEAREST);
+			GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MIN_FILTER, GL4.GL_NEAREST);
+			GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_S, GL4.GL_CLAMP_TO_EDGE);
+			GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_T, GL4.GL_CLAMP_TO_EDGE);
+			GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);	
+		}
 	}
 	private void SetupFramebuffer() {
 		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, fbo_id);
 		GLWrapper.glFramebufferTexture2D(
 				GL4.GL_FRAMEBUFFER, GL4.GL_COLOR_ATTACHMENT0, 
-				GL4.GL_TEXTURE_2D, output_texture_id, 0);
+				GL4.GL_TEXTURE_2D, output_texture_ids[0], 0);
+		GLWrapper.glFramebufferTexture2D(
+				GL4.GL_FRAMEBUFFER, GL4.GL_COLOR_ATTACHMENT1, 
+				GL4.GL_TEXTURE_2D, output_texture_ids[1], 0);
+		int[] draw_buffers=new int[] {GL4.GL_COLOR_ATTACHMENT0,GL4.GL_COLOR_ATTACHMENT1};
+		GLWrapper.glDrawBuffers(2, Buffers.newDirectIntBuffer(draw_buffers));
 		if(GLWrapper.glCheckFramebufferStatus(GL4.GL_FRAMEBUFFER)!=GL4.GL_FRAMEBUFFER_COMPLETE) {
 			System.out.println("Error:Incomplete framebuffer");
 		}
@@ -86,10 +94,17 @@ public class BoxMullerMethodWindow extends JOGLFWindow{
 	}
 	private void SetupProgram() {
 		GLShaderFunctions.CreateProgram(
-				"box_muller", 
-				"./Data/Shader/330/box_muller/vshader.glsl",
-				"./Data/Shader/330/box_muller/fshader.glsl");
-		program=new ShaderProgram("box_muller");
+				"tilde_h0k", 
+				"./Data/Shader/330/ocean/tilde_h0k/vshader.glsl",
+				"./Data/Shader/330/ocean/tilde_h0k/fshader.glsl");
+		program=new ShaderProgram("tilde_h0k");
+		
+		program.Enable();
+		program.SetUniform("N", 512);
+		program.SetUniform("L", 1000);
+		program.SetUniform("A", 4.0f);
+		program.SetUniform("v", 40.0f);
+		program.SetUniform("w", 1.0f, 1.0f);
 	}
 	
 	@Override
@@ -104,24 +119,35 @@ public class BoxMullerMethodWindow extends JOGLFWindow{
 		GLDrawFunctions2D.TransferFullscreenQuad();
 		GLWrapper.glBindFramebuffer(GL4.GL_FRAMEBUFFER, 0);
 		
-		this.OutputResult();
+		this.SaveResults();
 		this.CloseWindow();
 	}
-	private void OutputResult() {
+	private void SaveResults() {
 		int size=TEXTURE_WIDTH*TEXTURE_HEIGHT*4;
-		FloatBuffer data=Buffers.newDirectFloatBuffer(size);
+		FloatBuffer data0=Buffers.newDirectFloatBuffer(size);
+		FloatBuffer data1=Buffers.newDirectFloatBuffer(size);
 		
-		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, output_texture_id);
-		GLWrapper.glGetTexImage(GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA, GL4.GL_FLOAT, data);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, output_texture_ids[0]);
+		GLWrapper.glGetTexImage(GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA, GL4.GL_FLOAT, data0);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, output_texture_ids[1]);
+		GLWrapper.glGetTexImage(GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA, GL4.GL_FLOAT, data1);
 		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
 		
-		List<String> lines=new ArrayList<>(size);
-		for(int i=0;i<size;i++) {
-			lines.add(String.valueOf(data.get()));
+		List<String> data0_lines=new ArrayList<>(size);
+		List<String> data1_lines=new ArrayList<>(size);
+		
+		for(int i=0;i<size;i+=4) {
+			data0_lines.add(String.valueOf(data0.get(i)));
+			data0_lines.add(String.valueOf(data0.get(i+1)));
+			
+			data1_lines.add(String.valueOf(data1.get(i)));
+			data1_lines.add(String.valueOf(data1.get(i+1)));
 		}
 		
 		try {
-			FileFunctions.CreateTextFile("./Data/Text/normal_distribution.txt", "UTF-8", lines);
+			FileFunctions.CreateTextFile("./Data/Text/ocean/tilde_h0k.txt", "UTF-8", data0_lines);
+			FileFunctions.CreateTextFile("./Data/Text/ocean/tilde_h0minusk.txt", "UTF-8", data1_lines);
 		}
 		catch(IOException e) {
 			e.printStackTrace();
