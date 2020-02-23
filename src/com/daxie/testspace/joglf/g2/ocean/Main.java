e025ff5c19c6aedd7f9ae4004681ed0081857d02
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-02-23 14:23:55 +0900

    Removed files.

--- src/com/daxie/testspace/joglf/g2/ocean/Main.java
+++ src/com/daxie/testspace/joglf/g2/ocean/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		LogWriter.SetLogLevelFlags(LogWriter.LOG_LEVEL_ALL);
 		GLFront.Setup(GLVersion.GL4);
 		
-		JOGLFWindow window=new ButterflyTextureTestWindow2();
+		JOGLFWindow window=new JOGLFWindow();
 		window.SetExitProcessWhenDestroyed();
 	}
 }
